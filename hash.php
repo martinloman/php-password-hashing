24da1c43ab1a52f5acf1064bd0612fb29be658a4
martinloman <martin.loman@gmail.com>
2022-11-15 15:27:45 +0100

    More comments.

--- hash.php
+++ hash.php
@@ -1,10 +1,19 @@
 <?php
+//---------------------------------------
 // Exempel-kod, hashning
+//---------------------------------------
+
+// Se dokumentation för hash() som kan skapa hashvärden med hjälp av olika
+// hashningsalgoritmer. https://www.php.net/manual/en/function.hash.php
 echo "SHA256: " . hash("sha256", $_POST["password"]);
 echo "<br>";
 echo "MD5: " . hash("md5", $_POST["password"]);
 echo "<br>";
+
+// Se dokumentation för password_hash() https://www.php.net/manual/en/function.password-hash.php
 $hash_value = password_hash($_POST["password"], PASSWORD_DEFAULT);
 echo "Password hash: " . $hash_value;
 echo "<br>";
+
+// Se dokumentation för password_verify() https://www.php.net/manual/en/function.password-verify.php
 echo "Password ok: " . password_verify($_POST["password"], $hash_value);
